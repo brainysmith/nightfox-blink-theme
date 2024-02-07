174c640b56f131d77271fd5f09e21c6fab3dcd73
vkarpov <vkarpov@identityblitz.com>
2024-02-07 21:22:41 +0300

    Added a JavaScript theme file.

---
+++ nightfox.js
@@ -0,0 +1,5 @@
+// nightfox theme for Blink based on https://github.com/EdenEast/nightfox.nvim 
+t.prefs_.set('color-palette-overrides',["#393b44", "#c94f6d", "#81b29a", "#dbc074", "#719cd6", "#9d79d6", "#63cdcf", "#dfdfe0", "#575860", "#d16983", "#8ebaa4", "#e0c989", "#86abdc", "#baa1e2", "#7ad5d6", "#e4e4e5"]);
+t.prefs_.set('foreground-color', "#cdcecf");
+t.prefs_.set('background-color', "#192330");
+t.prefs_.set('cursor-color', 'rgba(205,206,207,0.5)');
